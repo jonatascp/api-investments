8e76882112ce97c069f04ade4edf63c2f5b83c92
jonatascp <jonatascp@gmail.com>
2017-07-03 19:48:38 -0300

    remove dependecy body-parser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var load = require('express-load');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 var app = express();
@@ -13,15 +12,10 @@ app.set('view engine', 'ejs');
 
 app.use(cookieParser('api-investments'));
 app.use(session());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(methodOverride());
 
 app.use(express.static(__dirname + '/public'));
 
-// Ao tentar configurar tratamento de erro, a tela sempre é exibida para qualquer requisicao.
-// app.use(error.notFound);
-// app.use(error.serverError);
 
 load('models').then('controllers').then('routes').into(app);
 
